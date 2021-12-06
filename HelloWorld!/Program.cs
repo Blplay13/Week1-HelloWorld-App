40be75950d36a03584f8f81b0c420ffc188cb0a4
Blplay13 <95048551+Blplay13@users.noreply.github.com>
2021-12-06 23:41:36 +0200

    Create Hello World Program

---
+++ HelloWorld!/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your name ?");
+            string UserName;
+            UserName = Console.ReadLine();
+
+            Console.WriteLine("Hello," +UserName);
+
+            Console.Read();
+        }
+    }
+}
